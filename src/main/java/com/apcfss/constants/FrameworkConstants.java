3bb1000118df87976ff72efd96713c025c16a155
sravan kumar M <1590@APCFSS-DT-088.DOMAIN.APCFSS.IN>
2025-03-20 18:04:43 +0530

    github token

--- src/main/java/com/apcfss/constants/FrameworkConstants.java
+++ src/main/java/com/apcfss/constants/FrameworkConstants.java
@@ -7,6 +7,7 @@ public final class FrameworkConstants {
 	public FrameworkConstants() {
 	}
 
+	//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 --> github token
 	private static final String RESOURCESPATH =  System.getProperty("user.dir")+"/src/test/resources";
 	private static final String CONFIGFILEPATH = RESOURCESPATH+"/config/FrameworkConfigurations.properties";
 	
