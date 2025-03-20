c4beb8e4fcaa7d19ef31f675db92962bb71e133f
sravan kumar M <1590@APCFSS-DT-088.DOMAIN.APCFSS.IN>
2025-03-20 18:06:25 +0530

    20th final commit

--- src/main/java/com/apcfss/constants/FrameworkConstants.java
+++ src/main/java/com/apcfss/constants/FrameworkConstants.java
@@ -7,7 +7,6 @@ public final class FrameworkConstants {
 	public FrameworkConstants() {
 	}
 
-	//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 --> github token
 	private static final String RESOURCESPATH =  System.getProperty("user.dir")+"/src/test/resources";
 	private static final String CONFIGFILEPATH = RESOURCESPATH+"/config/FrameworkConfigurations.properties";
 	
